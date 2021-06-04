334d01b0755cb90a9c36c96864c8a59948c2cf53
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-06-03 22:41:12 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func test1(x int) int {
+	return x + 1
+}
+
+
+func test4(p_func func(int) int){
+	fmt.Println(p_func(40))
+}
+
+
+func returnFunc(x string) func(){
+	return func() { fmt.Println(x) }
+}
+
+
+func main() {	
+	fmt.Println(test1(10))
+
+
+	test2 := func(x int) int {
+		return x + 2
+	}
+	fmt.Println(test2(20))
+
+
+	test3 := func(x int) int {
+		return x + 3
+	}(30)
+
+	fmt.Println(test3)
+
+
+	test4(test2)
+
+	returnFunc("hello")()
+	returnFunc("world")()
+
+	y := returnFunc("goodbye")
+	y()
+
+	//fmt.Println("x=", x, " y=", y, "z=", z)
+
+}
